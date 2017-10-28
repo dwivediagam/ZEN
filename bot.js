479a647914ed3b225e21e235876b2a6ac8e2625f
CodHeK <gaganganapathyas@yahoo.in>
2017-10-29 05:06:47 +0530

    new ui

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$("#buttons").click(function(e){
+	$("#form").submit(function(e){
 		var search = $("#msg").val();
 		search = search.split(" ");
 		var stringArray = new Array();
@@ -26,33 +26,14 @@ $(document).ready(function() {
 				var StringOutput = "", imgLink = "";
 				$("#result").empty();
 				for(var j=0;j<data.queryresult.pods.length;j++) {
-					// console.log(data.queryresult.pods[j].title);
-					// if(data.queryresult.pods[j].title == "Image") {
-					// 	imgLink = data.queryresult.pods[j].subpods[0].img.src;
-					// 	break;
-					// }
 
 					$("#result").append(data.queryresult.pods[j].markup.data);
 				}
 				
-				//alert(imgLink);
-				// if(data.queryresult.pods[1].subpods[0].plaintext == "") {
-				// 	StringOutput = data.queryresult.pods[0].subpods[0].plaintext;
-				// 	$("#list").append('<li> <div class="jumbotron"><h3><b> Gags:&nbsp</b><div class="row" style="width:100%;height:75%;margin-top:3%;"><ul style="list-style:none;">' + StringOutput + '</ul></div></div></li>').fadeIn(2000);
-				// }
-				// else {
-				// 	StringOutput = data.queryresult.pods[1].subpods[0].plaintext;
-				// 	$("#list").append('<li> <div class="jumbotron"><h3><b> Gags:&nbsp</b><div class="row" style="width:100%;height:75%;margin-top:3%;"><ul style="list-style:none;">' + StringOutput + '</ul></div></div></li>').fadeIn(2000);
-				// 	//$("#list").append('<li> <div class="jumbotron"><h3><b> Gags:&nbsp</b><div class="row" style="width:100%;height:75%;margin-top:3%;"><ul style="list-style:none;"><img src=' + imgLink + '></ul></div></div></li>').fadeIn(2000);
-				// }
 				
 				
 			}
 		});
-
-		function getOutput(data) {
-
-		}
 		e.preventDefault();
 	});
 });
