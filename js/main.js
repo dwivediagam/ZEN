ab36df1ae392010ec22e3817f44f9aa56bb65176
CodHeK <gaganganapathyas@yahoo.in>
2017-10-29 11:12:00 +0530

    Resolved errors

--- js/main.js
+++ js/main.js
@@ -68,7 +68,8 @@ function sendFileToCloudVision (content) {
     data: JSON.stringify(request),
     contentType: 'application/json'
   }).fail(function (jqXHR, textStatus, errorThrown) {
-    $('#result').append('<h2 style="text-align:centre;font-size:40px;color:black;font-family: 'Saira', sans-serif;">TEXT NOT FOUND :(</h2>');
+     $('#result').text('ERRORS: ' + textStatus + ' ' + errorThrown);
+    // $('#result').append('<h2 style="text-align:centre;font-size:40px;color:black;font-family: 'Saira', sans-serif;">TEXT NOT FOUND :(</h2>');
   }).done(displayJSON);
 }
 
